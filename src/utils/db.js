4e11510ce7906cd47097bb10bcf8b55b2c49c10a
bodiabeliy <bboda2106@gmail.com>
2025-06-06 20:02:12 +0300

    reconfig test database

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,40 +1,40 @@
 
 export const categoryTopicsList =[
     {
-        image:`${process.env.REACT_APP_BASE_URL}/wild%20nature/topicImage.jpg`,
-        imageTemplate:`${process.env.REACT_APP_BASE_URL}/wild%20nature/wild_nature.mind`,
-        userExample:`${process.env.REACT_APP_BASE_URL}/wild%20nature/animals_template.png`,
+        image:`https://soundgame-server.onrender.com/wild%20nature/topicImage.jpg`,
+        imageTemplate:`https://soundgame-server.onrender.com/wild%20nature/wild_nature.mind`,
+        userExample:`https://soundgame-server.onrender.com/wild%20nature/animals_template.png`,
         topicName:"Дика природа",
         topicItems:[
             {
                 parentTopic: "Дика природа",
                 itemName: "frog",
                 targetIndex: 0,
-                audio: `${process.env.REACT_APP_BASE_URL}/wild%20nature/frog/frog.mp3`,
+                audio: `https://soundgame-server.onrender.com/wild%20nature/frog/frog.mp3`,
             },
             {
                 parentTopic: "Дика природа",
                 itemName: "eagle",
                 targetIndex: 1,
-                audio: `${process.env.REACT_APP_BASE_URL}/wild%20nature/eagle/eagle.mp3`,
+                audio: `https://soundgame-server.onrender.com/wild%20nature/eagle/eagle.mp3`,
             },
             {
                 parentTopic: "Дика природа",
                 itemName: "snake",
                 targetIndex: 2,
-                audio: `${process.env.REACT_APP_BASE_URL}/wild%20nature/snake/snake.mp3`,
+                audio: `https://soundgame-server.onrender.com/wild%20nature/snake/snake.mp3`,
             },
             {
                 parentTopic: "Дика природа",
                 itemName: "lion",
                 targetIndex: 3,
-                audio: `${process.env.REACT_APP_BASE_URL}/wild%20nature/lion/lion.mp3`,
+                audio: `https://soundgame-server.onrender.com/wild%20nature/lion/lion.mp3`,
             },
             {
                 parentTopic: "Дика природа",
                 itemName: "owl",
                 targetIndex: 4,
-                audio: `${process.env.REACT_APP_BASE_URL}/wild%20nature/owl/owl.mp3`,
+                audio: `https://soundgame-server.onrender.com/wild%20nature/owl/owl.mp3`,
             },
              
             
@@ -43,28 +43,28 @@ export const categoryTopicsList =[
         ]
     },
     {
-        image:`${process.env.REACT_APP_BASE_URL}/tools/toolsTopic.png`,
-        imageTemplate:`${process.env.REACT_APP_BASE_URL}/tools/tools.mind`,
-        userExample:`${process.env.REACT_APP_BASE_URL}/tools/toolsTopic.png`,
+        image:`https://soundgame-server.onrender.com/tools/toolsTopic.png`,
+        imageTemplate:`https://soundgame-server.onrender.com/tools/tools.mind`,
+        userExample:`https://soundgame-server.onrender.com/tools/toolsTopic.png`,
         topicName:"Інструменти",
         topicItems:[
             {
                 parentTopic: "Інструменти",
                 itemName: "drill",
                 targetIndex: 0,
-                audio: `${process.env.REACT_APP_BASE_URL}/tools/drill/drill.mp3`,
+                audio: `https://soundgame-server.onrender.com/tools/drill/drill.mp3`,
             },
             {
                 parentTopic: "Інструменти",
                 itemName: "hummer",
                 targetIndex: 1,
-                audio: `${process.env.REACT_APP_BASE_URL}/tools/hummer/hummer.mp3`,
+                audio: `https://soundgame-server.onrender.com/tools/hummer/hummer.mp3`,
             },
              {
                 parentTopic: "Інструменти",
                 itemName: "jackhummer",
                 targetIndex: 3,
-                audio: `${process.env.REACT_APP_BASE_URL}/tools/jackhummer/jackhummer.mp3`,
+                audio: `https://soundgame-server.onrender.com/tools/jackhummer/jackhummer.mp3`,
             }
           
         ]
